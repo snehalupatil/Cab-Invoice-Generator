93496778223e6eab2e4fe0f36dbe1ab37c9c8eb4
snehalupatil <patilsnehal4575@gmail.com>
2021-05-28 13:38:33 +0530

    Added: Calculate Minimum Fare of Ride

--- main/java/InvoiceGenerator.java
+++ main/java/InvoiceGenerator.java
@@ -2,9 +2,13 @@ public class InvoiceGenerator {
     //constants
     private static final int COST_PER_MIN_TIME = 1;
     private static final double  MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
 
     //method to calculate and return total fare
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MIN_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MIN_TIME;
+        if(totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
